29c7f1a50fbb082aa57b4d4868f593dcc822934c
Liu Wei <liuwei@laiye.com>
2020-12-08 02:47:52 +0000

    upload scripts

--- test_reader_me.sh
+++ test_reader_me.sh
@@ -5,14 +5,14 @@ export CUDA_VISIBLE_DEVICES=0
 
 python -m torch.distributed.launch \
     --nproc_per_node=1 train_reader.py \
-    --prediction_results_file ./data/dr_data/reader/test_results.json.dpr \
-    --dev_file ./data/dr_data/reader/test.json \
-    --eval_top_docs 5 \
-    --model_file ./data/dr_exp/reader/dpr_reader.19.5119 \
-    --dev_batch_size 200 \
-    --passages_per_question_predict 5 \
-    --sequence_length 320  \
-    --max_answer_length 100 \
-    --fp16 \
-    --fp16_opt_level O2 \
-    --test_only 
+    --prediction_results_file ./data/dr_data/reader/dev_results.json.dpr \
+    --dev_file ./data/dr_data/reader/dev.json\
+    --eval_top_docs 1 \
+    --model_file ./data/dr_exp/reader/dpr_reader.0.10000 \
+    --dev_batch_size 40 \
+    --passages_per_question_predict 1 \
+    --sequence_length 512 \
+    --max_answer_length 500 \
+    # --test_only 
+    # --fp16 \
+    # --fp16_opt_level O2 \
--- train_reader_me.sh
+++ train_reader_me.sh
@@ -3,28 +3,27 @@ export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=0,1
 
 
-python -m torch.distributed.launch \
-    --nproc_per_node=2 train_reader.py \
+python train_reader.py \
     --seed 42 \
     --max_n_answers 1 \
-    --passages_per_question 3 \
-    --passages_per_question_predict 10 \
-    --max_answer_length 200 \
-    --eval_top_docs 10 \
+    --passages_per_question 1 \
+    --passages_per_question_predict 1 \
+    --max_answer_length 500 \
+    --eval_top_docs 1 \
     --learning_rate 1e-5 \
-    --eval_step 6000 \
+    --eval_step 10000 \
+    --warmup_steps 10000 \
     --encoder_model_type hf_bert \
-    --pretrained_model_cfg ./models/RoBERTa-wwm-ext \
+    --pretrained_model_cfg bert-base-chinese \
     --do_lower_case \
-    --train_file data/dr_data/reader/train.json \
     --dev_file data/dr_data/reader/dev.json \
-    --warmup_steps 0 \
-    --sequence_length 320 \
-    --num_train_epochs 100 \
-    --batch_size 8 \
-    --dev_batch_size 8 \
+    --train_file data/dr_data/reader/train.json \
+    --sequence_length 512 \
+    --num_train_epochs 10 \
+    --batch_size 16 \
+    --dev_batch_size 16 \
     --output_dir data/dr_exp/reader \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 1 \
     > data/dr_exp/reader/train.log 
-    #--fp16 \
-    #--fp16_opt_level O2 \
+    # --fp16 \
+    # --fp16_opt_level O2 \
