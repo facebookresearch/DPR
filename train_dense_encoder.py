25f8d38fcbfe0ecec281d377a490a554a8d20680
Yuan Zhang <zhangyuan2@laiye.com>
2020-12-14 13:05:01 +0800

    fix

--- train_dense_encoder.py
+++ train_dense_encoder.py
@@ -38,7 +38,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 if (logger.hasHandlers()):
     logger.handlers.clear()
-console = logging.StreamHandler()
+console = logging.StreamHandler(stream=sys.stdout)
 logger.addHandler(console)
 
 
