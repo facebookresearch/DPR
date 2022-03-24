12b1fae99cd0bef6707a84d7027cb8b520de9e2b
ola13 <ola.piktus@gmail.com>
2022-03-24 07:55:42 -0700

    fix spacy deps

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "transformers>=3.0.2,<3.1.0",
         "tqdm>=4.27",
         "wget",
-        "spacy>=2.1.8",
+        "spacy==2.1.8",
         "hydra-core>=1.0.0",
         "omegaconf>=2.0.1",
     ],
